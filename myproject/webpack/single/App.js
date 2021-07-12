a6518f5556628d809d614b145bb64e9777981c40
Tomato8pepper <2798645458@qq.com>
2021-07-12 21:41:13 +0800

    webpack

---
+++ myproject/webpack/single/App.js
@@ -0,0 +1,4 @@
+var App = {
+    template: '<div>这是一个入口组件</div>'
+}
+export default App;
---
+++ myproject/webpack/single/main.js
@@ -0,0 +1,10 @@
+import Vue from './vue.js'
+import App from './App.js'
+
+new Vue({
+    el: '#app',
+    template: '<div><App /></div>',
+    components: {
+        App
+    }
+})
